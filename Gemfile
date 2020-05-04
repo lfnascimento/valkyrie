1aeb96a446413851389b8c78c5825a5e21644bc5
Luis Fernando Nascimento <nascimento.lsfo@gmail.com>
2020-05-03 22:17:52 -0300

    Add Best Available Seats
    Add Operation
    Config Rspec
    Spec

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-byebug', '~> 3.8'
 end
 
 group :development do
@@ -39,6 +40,12 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+group :test do
+  gem 'rspec-rails', '~> 3.9.0'
+  gem 'simplecov', '~> 0.17.1', require: false
+  gem 'shoulda-matchers', '~> 4.3.0'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ app/operations/seats/best_available_seats_operation.rb
@@ -0,0 +1,84 @@
+module Seats
+  class BestAvailableSeatsOperation
+    attr_reader :venue, :party_of
+
+    def initialize(venue, party_of = 1)
+      @venue = venue
+      @party_of = party_of
+      @best_available_seats = []
+    end
+
+    def perform
+      venue[:seats].values.each(&method(:calculate_distance_from_best_seats))
+      if party_of > 1
+        venue[:seats].values.group_by { |seat| seat[:row] }.values.each(&method(:lookup_best_consecutive_seats))
+        best_available_seats
+      else
+        best_available_seats.push(venue[:seats].values.min_by { |seat| seat[:distance] })
+      end
+    end
+
+    private
+
+    attr_reader :best_available_seats
+
+    def lookup_best_consecutive_seats(row)
+      return if party_of > row.size
+
+      check_consecutive_seats(row.sort_by { |seat| seat[:distance] })
+    end
+
+    def check_consecutive_seats(row)
+      while party_of <= row.size do
+        break if set_best_consecutive_available_seats(row.first(party_of))
+        row.shift
+      end
+    end
+
+    def set_best_consecutive_available_seats(seats)
+      @best_available_seats = seats if best_consecutive_available_seats?(seats)
+    end
+
+    def best_consecutive_available_seats?(seats)
+      consecutive_seats?(seats) &&
+        ((seats.sum(0) { |seat| seat[:distance] } < best_available_seats.sum(0) { |seat| seat[:distance] }) ||
+         best_available_seats.empty?)
+    end
+
+    def consecutive_seats?(seats)
+      return false if seats.size != party_of
+
+      columns = seats.map { |seat| seat[:column] }
+      columns.max - columns.min + 1 == columns.size
+    end
+
+    def calculate_distance_from_best_seats(seat)
+      seat[:distance] = best_seats.map do |best_seat|
+        calculate_distance(best_seat[:col],
+                           best_seat[:row],
+                           seat[:column].to_i,
+                           letter_to_number(seat[:row]))
+      end.min
+    end
+
+    def calculate_distance(best_seat_col, best_seat_row, seat_col, seat_row)
+      Math.sqrt(((best_seat_col - seat_col) ** 2) + ((best_seat_row - seat_row) ** 2))
+    end
+
+    def best_seats
+      @best_seats ||= calculate_best_seat_columns.map { |col| { row: 1, col: col } }
+    end
+
+    def calculate_best_seat_columns
+      row_size.even? ? [(row_size / 2), ((row_size / 2) + 1)] : [(row_size / 2.0).ceil]
+    end
+
+    def row_size
+      @row_size ||= venue.dig(:venue, :layout, :rows).to_i
+    end
+
+    def letter_to_number(letter)
+      letter.upcase.tr('A-Z', '1-9a-q').to_i(27)
+    end
+  end
+end
