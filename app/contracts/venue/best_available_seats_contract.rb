5dea1b28ab580ea9e5cad2bfb07e9f1526cbe2cc
Luis Fernando Nascimento <nascimento.lsfo@gmail.com>
2020-05-04 23:54:16 -0300

    Fix contract locale
    Improving contract validations

--- app/contracts/venue/best_available_seats_contract.rb
+++ app/contracts/venue/best_available_seats_contract.rb
@@ -5,24 +5,24 @@ module Venue
     params do
       required(:venue).hash do
         required(:layout).hash do
-          required(:rows).filled(:integer, gt?: 1)
-          required(:columns).filled(:integer, gt?: 1)
+          required(:rows).value(:integer, gt?: 1, lt?: 27)
+          required(:columns).value(:integer, gt?: 1, lt?: 100)
         end
       end
 
       required(:seats).array(:hash)  do
         required(:row).filled(:string)
-        required(:column).filled(:integer, gt?: 1)
+        required(:column).value(:integer, gt?: 1)
       end
     end
 
     rule(:seats).each do
       if value[:column] > values[:venue][:layout][:columns]
-        key.failure(I18n.t('contracts.best_available_seats.column_lt_or_eq_layout'))
+        key.failure(I18n.t('contracts.venue.best_available_seats.column_lt_or_eq_layout_size'))
       end
 
       if letter_to_number(value[:row]) > values[:venue][:layout][:rows]
-        key.failure(I18n.t('contracts.best_available_seats.column_lt_or_eq_layout'))
+        key.failure(I18n.t('contracts.venue.best_available_seats.row_lt_or_eq_layout_size'))
       end
     end
   end
