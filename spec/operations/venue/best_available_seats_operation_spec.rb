759aa73022841f37a1064c20c6551960c17ec2c5
Luis Fernando Nascimento <nascimento.lsfo@gmail.com>
2020-08-02 00:28:44 -0300

    Fix when there are more than one group of consec seats on the same row

--- spec/operations/venue/best_available_seats_operation_spec.rb
+++ spec/operations/venue/best_available_seats_operation_spec.rb
@@ -5,7 +5,7 @@ describe Venue::BestAvailableSeatsOperation, type: :operation do
 
   describe '#perform' do
     context 'party of 1' do
-      context 'venue with a odd number of columns size' do
+      context 'venue with a odd number of columns' do
         context 'when the best seat is not available' do
           let(:request) do
             JSON.parse(File.read('spec/support/files/venue/venue_with_odd_column.json'), symbolize_names: true)
@@ -26,7 +26,8 @@ describe Venue::BestAvailableSeatsOperation, type: :operation do
 
         context 'when the best seat is available' do
           let(:request) do
-            JSON.parse(File.read('spec/support/files/venue/venue_with_odd_column_and_when_the_best_seat_is_available.json'), symbolize_names: true)
+            JSON.parse(File.read('spec/support/files/venue/venue_with_odd_column_and_when_the_best_seat_is_available.json'),
+                       symbolize_names: true)
           end
 
           it do
@@ -43,7 +44,7 @@ describe Venue::BestAvailableSeatsOperation, type: :operation do
         end
       end
 
-      context 'venue with a even number of columns size' do
+      context 'venue with a even number of columns' do
         let(:request) do
           JSON.parse(File.read('spec/support/files/venue/venue_with_even_column.json'), symbolize_names: true)
         end
@@ -60,32 +61,65 @@ describe Venue::BestAvailableSeatsOperation, type: :operation do
           ]
         end
       end
+    end
 
-      context 'party of 2' do
-        context 'venue with a even number of columns size and consecutive seats available' do
-          let(:request) do
-            JSON.parse(File.read('spec/support/files/venue/venue_with_even_column_and_consecutive_seats_available.json'),
-                       symbolize_names: true)
-          end
+    context 'party of greater than 1' do
+      context 'venue with a even number of columns and consecutive availabel seats' do
+        let(:request) do
+          JSON.parse(File.read('spec/support/files/venue/venue_with_even_column_and_consecutive_seats_available.json'),
+                     symbolize_names: true)
+        end
 
-          it do
-            expect(subject.perform).to eq [
-              {
-                column: 4,
-                distance: 2.0,
-                id: "c4",
-                row: "c",
-                status: "AVAILABLE"
-              },
-              {
-                column: 5,
-                distance: 2.23606797749979,
-                id: "c5",
-                row: "c",
-                status: "AVAILABLE"
-              }
-            ]
-          end
+        it do
+          expect(subject.perform).to eq [
+            {
+              column: 5,
+              distance: 2.0,
+              id: "c5",
+              row: "c",
+              status: "AVAILABLE"
+            },
+            {
+              column: 6,
+              distance: 2.0,
+              id: "c6",
+              row: "c",
+              status: "AVAILABLE"
+            }
+          ]
+        end
+      end
+
+      context 'venue with a odd number of columns and more than one group of consecutive available seats on the same row' do
+        let(:request) do
+          JSON.parse(File.read('spec/support/files/venue/with_an_odd_number_of_columns_and_more_than_one_group_of_consecutive_available_seats_on_the_same_row.json'),
+                     symbolize_names: true)
+        end
+
+        it do
+          expect(subject.perform).to eq [
+            {
+              id: "b6",
+              row: "b",
+              column: 6,
+              distance: 1.4142135623730951,
+              status: "AVAILABLE"
+            },
+            {
+              id: "b7",
+              row: "b",
+              column: 7,
+              distance: 2.23606797749979,
+              status: "AVAILABLE"
+            },
+            {
+              id: "b8",
+              row: "b",
+              distance: 3.1622776601683795,
+              column: 8,
+              status: "AVAILABLE"
+            }
+          ]
         end
       end
     end
