0fd90dbd8b02d537f82be9f414301f8fff88beaa
Luis Fernando Nascimento <nascimento.lsfo@gmail.com>
2020-08-01 21:27:03 -0300

    Fix best seat column

--- spec/operations/venue/best_available_seats_operation_spec.rb
+++ spec/operations/venue/best_available_seats_operation_spec.rb
@@ -6,26 +6,46 @@ describe Venue::BestAvailableSeatsOperation, type: :operation do
   describe '#perform' do
     context 'party of 1' do
       context 'venue with a odd row size' do
-        let(:request) do
-          JSON.parse(File.read('spec/support/files/venue/venue_with_odd_row.json'), symbolize_names: true)
+        context 'when the best seat is not available' do
+          let(:request) do
+            JSON.parse(File.read('spec/support/files/venue/venue_with_odd_column.json'), symbolize_names: true)
+          end
+
+          it do
+            expect(subject.perform).to eq([
+              {
+                column: 5,
+                distance: 1.4142135623730951,
+                id: "b5",
+                row: "b",
+                status: "AVAILABLE"
+              }
+            ])
+          end
         end
 
-        it do
-          expect(subject.perform).to eq([
-            {
-              column: 5,
-              distance: 1.4142135623730951,
-              id: "b5",
-              row: "b",
-              status: "AVAILABLE"
-            }
-          ])
+        context 'when the best seat is available' do
+          let(:request) do
+            JSON.parse(File.read('spec/support/files/venue/venue_with_odd_column_and_when_the_best_seat_is_available.json'), symbolize_names: true)
+          end
+
+          it do
+            expect(subject.perform).to eq([
+              {
+                column: 5,
+                distance: 0,
+                id: "a5",
+                row: "a",
+                status: "AVAILABLE"
+              }
+            ])
+          end
         end
       end
 
       context 'venue with a even row size' do
         let(:request) do
-          JSON.parse(File.read('spec/support/files/venue/venue_with_even_row.json'), symbolize_names: true)
+          JSON.parse(File.read('spec/support/files/venue/venue_with_even_column.json'), symbolize_names: true)
         end
 
         it do
@@ -44,7 +64,7 @@ describe Venue::BestAvailableSeatsOperation, type: :operation do
       context 'party of 2' do
         context 'venue with a even row size and consecutive seats available' do
           let(:request) do
-            JSON.parse(File.read('spec/support/files/venue/venue_with_even_row_and_consecutive_seats_available.json'),
+            JSON.parse(File.read('spec/support/files/venue/venue_with_even_column_and_consecutive_seats_available.json'),
                        symbolize_names: true)
           end
 
